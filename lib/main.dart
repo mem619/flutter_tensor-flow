8e1c3e06501e2c47c133a2165c0d0bde163bbd9e
Jose Rios <mem619@hotmail.com>
2020-10-03 20:36:33 -0500

    integracion con tensor flow.

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,193 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:tflite/tflite.dart';
+import 'package:image_picker/image_picker.dart';
+
+
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    title: 'Simple Interest Calculator App',
+    home: FormFlutter(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class FormFlutter extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  State<StatefulWidget> createState() {
+    return _FormFlutterState();
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class _FormFlutterState extends State<FormFlutter> {
+  var _currencies = ['Rupees','Dollars','Pounds','Pesos'];
+  final _minimumPadding = 5.0;
+  //variables tensorflow
+  File _image;
+  final picker = ImagePicker();
+  List _output;
+  bool _state = false;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+  void initState() {
+    loadModel();
+    super.initState();
+  }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  //funtions tensorflow
+  getImage()async{
+    final pickedFile = await picker.getImage(source: ImageSource.gallery); //Select gellery webcam
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      if(pickedFile != null){
+        _image= File(pickedFile.path);
+        classifyImage(pickedFile);
+      }else {
+        print('No se encontro la imagen');
+      }
     });
   }
 
+  //funcion cargar modelo de tensorflow
+  loadModel() async{
+    await Tflite.loadModel(
+      model: 'assets/model_unquant.tflite',
+      labels: 'assets/labels.txt'
+    );
+  }
+
+  //Funcion Clasificar
+  classifyImage(image) async{
+    var output = await Tflite.runModelOnImage(
+      path: image.path,
+      numResults: 2,
+      threshold: 0.5,
+      imageMean: 127.5,
+      imageStd: 127.5
+    );
+
+    setState(() {
+      _output = output;
+      _state = true;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+          title: Text('Example Flutter TensorFlow')
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Container(
+        margin: EdgeInsets.all(_minimumPadding * 2),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            getImageAsset(),
+            Padding(padding: EdgeInsets.only(top: _minimumPadding, bottom: _minimumPadding),
+            child: TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Principal',
+                hintText: 'Enter Principal e.g. 12000',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5.0)
+                )
+              ),
+            ))
+            ,
+            Padding(padding: EdgeInsets.only(top: _minimumPadding, bottom: _minimumPadding),
+                child:TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Rate of interest',
+                  hintText: 'In percent',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0)
+                  )
+                ),
+              )
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Padding(
+              padding: EdgeInsets.only(top: _minimumPadding, bottom: _minimumPadding),
+              child: Row(
+              children: <Widget>[
+                Expanded( child: TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: 'Time',
+                    hintText: 'Time in years',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0)
+                    )
+                  ),
+                )),
+                Expanded( child: DropdownButton<String>(
+                  items: _currencies.map((String value) => DropdownMenuItem<String>(value:value, child: Text(value))).toList(),
+                  onChanged: (String newValueSelected) {
+
+                  },
+                  value: 'Pesos',
+                )),
+              ],
+            )),
+            Padding(
+              padding: EdgeInsets.only(top: _minimumPadding, bottom: _minimumPadding),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: _state == false? Container(
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(),
+                    ):
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      child: Column(
+                        children: <Widget>[
+                          _image == null ?
+                            Container():
+                            Image.file(_image),
+                          SizedBox(height: 30),
+                          _output != null ?
+                            Text(
+                              "${_output[0]['label']}",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 20.0
+                              ),
+                            ):
+                            Container()
+                        ],
+                      ),
+                    )
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: (){
+            getImage();
+        },
+        child: Icon(Icons.image),
+      ),
+    );
+  }
+
+  Widget getImageAsset() {
+    AssetImage assetImage = AssetImage('images/img_home.png');
+    Image image = Image(
+      image: assetImage,
+      width: 125,
+      height: 125,
+    );
+
+    return Container(
+      child: image,
+      margin: EdgeInsets.all(_minimumPadding * 10),
     );
   }
 }
